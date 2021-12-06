3ffe695b519e1aab0d29df27c3ec9f291b1cdd8d
hoon <skawlgns@kakao.com>
2021-12-06 15:12:06 +0900

    Layout & timerMode

--- StudyTimer/ViewController.swift
+++ StudyTimer/ViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+enum TimerState{
+    case start
+    case pause
+    case end
+}
 
 
 class ViewController: UIViewController {
@@ -19,21 +24,60 @@ class ViewController: UIViewController {
     
     var duration = 60
     var timer: DispatchSourceTimer?
-    
+    var timerState: TimerState = .end
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.configureToggleBtn()
     }
 
     @IBAction func tapToggleBtn(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration)
+       
         
+        switch self.timerState {
+        case .end:
+            self.datePicker.isHidden = true
+            self.outletHidden(isHidden: false)
+            self.cancleBtn.isEnabled = true
+            self.toggleBtn.isSelected = true
+            self.timerState = .start
+        case .start:
+            self.toggleBtn.isSelected = false
+            self.timerState = .pause
+            
+        case .pause:
+            self.toggleBtn.isSelected = true
+            self.timerState = .start
+        default:
+            break
+        }
     }
     
     @IBAction func tapCancleBtn(_ sender: UIButton) {
         
+        switch self.timerState {
+        case .start, .pause:
+            self.datePicker.isHidden = false
+            self.outletHidden(isHidden: true)
+            self.cancleBtn.isEnabled = false
+            self.toggleBtn.isSelected = false
+            self.timerState = .end
+        default:
+            break
+        }
     }
     
+    
+    func outletHidden(isHidden: Bool) {
+        self.timeLabel.isHidden = isHidden
+        self.progressView.isHidden = isHidden
+    }
+    
+    func configureToggleBtn() {
+        self.toggleBtn.setTitle("일시정지", for: .selected)
+        self.toggleBtn.setTitle("시작", for: .normal)
+    }
     
 }
 
